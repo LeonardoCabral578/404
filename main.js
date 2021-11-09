879a0bf9493849c269789f421f85c9d97b95d581
CabraLeo <leo.cabral1016@gmail.com>
2021-11-08 22:10:50 -0300

    2

---
+++ main.js
@@ -0,0 +1,7 @@
+   // Your application has indicated there's an error
+    window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.href = "https://www.google.co.in";
+
+    }, 5000);
