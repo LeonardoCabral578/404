a541cc1aaef8f14633e57b3dad33a862606c1ef4
CabraLeo <leo.cabral1016@gmail.com>
2021-11-08 22:24:49 -0300

    5

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,7 @@ var totalTime = 5;
 function updateClock() {
   document.getElementById('countdown').innerHTML = totalTime;
   if(totalTime==0){
-        window.location.href = "https://m.youtube.com/watch?v=umMqQ5_h63M&feature=youtu.be";
+    window.location.href = "https://m.youtube.com/watch?v=umMqQ5_h63M&feature=youtu.be";
   }else{
     totalTime-=1;
     setTimeout("updateClock()",1000);
